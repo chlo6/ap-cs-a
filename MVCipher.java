76b66b4a604532bb84e3cad9b9bca3e63f4ee3d1
chloe <162272068+chlo6@users.noreply.github.com>
2024-09-23 12:54:47 -0700

    Add files via upload

--- MVCipher.java
+++ MVCipher.java
@@ -1,22 +1,34 @@
-// imports go here
+import java.util.Scanner;
+import java.io.PrintWriter;
 
 /**
  *	MVCipher - Add your description here
  *	Requires Prompt and FileUtils classes.
  *	
- *	@author	
- *	@since	
+ *	@author	Chloe He	
+ *	@since	September 23, 2024
  */
  
  // diff (cat Macbeth.txt) (cat notcrypted.txt)
+ // add file utils
+ //		System.out.println("\nThe encrypted file " + outputFile + "has been created using the keyword -> " + key.toUpperCase());
+ 
  
  
 public class MVCipher {
 	
-	// fields go here
+	private	String	key;	// key used for encoding/decoding
+	private	String	inputLine = "";
+	private	final	int	MIN_KEY_LEN; // minimum key length needed for input
+	private	final	int	ENCRYPT_CHOICE; // number input for selecting encrypt	
+	
 		
 	/** Constructor */
-	public MVCipher() { }
+	public MVCipher() { 
+		key = "";
+		MIN_KEY_LEN = 3;
+		ENCRYPT_CHOICE = 1;
+	}
 	
 	public static void main(String[] args) {
 		MVCipher mvc = new MVCipher();
@@ -29,26 +41,62 @@ public class MVCipher {
 	public void run() {
 		System.out.println("\n Welcome to the MV Cipher machine!\n");
 		
-		/* Prompt for a key and change to uppercase
-		   Do not let the key contain anything but alpha
-		   Use the Prompt class to get user input */
 		
+		// getting key's input
+		String key = "";
+		while (key.length() <= MIN_KEY_LEN) {
+			key = Prompt.getString("Please input a word to use as key (letters only)");
+			if (key.length() <= MIN_KEY_LEN) {
+				System.out.println("ERROR: Key must be all letters and at least 3 characters long");
+			}
+		} 
+		System.out.println();
 		
-		/* Prompt for encrypt or decrypt */
-			
-			
-		/* Prompt for an input file name */
+		int choice = Prompt.getInt("Encrypt or decrypt? (1, 2)");
+				
+		readAndWrite();
 		
-		
-		/* Prompt for an output file name */
-		
-		
-		/* Read input file, encrypt or decrypt, and print to output file */
-		
-		
-		/* Don't forget to close your output file */
+		System.out.println("\nThe encrypted file " + outputFile + "has been created using the keyword -> " + key.toUpperCase());
 	}
 	
-	// other methods go here
+	public void readAndWrite() {
+		String inputFile, outputFile;
+		if (choice == ENCRYPT_CHOICE) inputFile = Prompt.nextString("Name of file to encrypt");
+		else inputFile = Prompt.nextString("Name of file to decrypt");
+		outputFile = Prompt.nextString("Name of output file");
+
+
+		Scanner input = FileUtils.openToRead(inputFile);
+		PrintWriter output = FileUtils.openToWrite(inputFile);
+		
+		while (input.hasNextLine()) {
+			inputLine = input.nextLine();
+			output.println(cryptString(inputLine));
+		}
+		
+		output.close();
+	}
 	
+	public String cryptString(String line) {
+		String returnline = "";
+		for (int i = 0; i < line.length(); i++) {
+			char ch = line.charAt(i);
+			int ind = (int)(ch);
+			if (ind >= 97 && ind <= 122) returnline += (char)(cryptLowerChar(ind, i));
+			else if (ind >= 65 && ind <= 90) returnline += (char)(cryptUpperChar(ind, i)); 
+		}
+		return returnline;
+	}
+	
+	public int cryptLowerChar(int ind, int keyind) {
+		int newind = ind + keyind;
+		if (newind > 122) newind -= 26;
+		return (newind);
+	}
+	
+	public int cryptUpperChar(int ind, int keyind) {
+		int newind = ind + keyind;
+		if (newind > 90) newind -= 26;
+		return (newind);
+	}
 }
