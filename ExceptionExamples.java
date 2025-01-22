3ed5a805f49cb4f166a4b501d6567fbc2563cd67
chloe <162272068+chlo6@users.noreply.github.com>
2025-01-22 14:09:28 -0800

    Add files via upload

---
+++ ExceptionExamples.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ConcurrentModificationException;
+/**
+ * The APCS exam tests for the following exceptions.
+ *	- ArithmeticException
+ *	- NullPointerException
+ *	- IndexOutOfBoundsException
+ *	- ArrayIndexOutOfBoundsException
+ *	- ConcurrentModificationException
+ */
+
+public class ExceptionExamples {
+	public static void main(String[] args) {
+		ExceptionExamples ee = new ExceptionExamples();
+		ee.run();
+	}
+	
+	public void run() {
+		arithmetic();
+		nullPointer();
+		indexBounds();
+		arrayIndexBounds();
+		concurrentModification();
+	}
+	
+	public void arithmetic() {
+		int x = 1;
+		int y = 0;
+		try {
+			int z = x / y;
+		} 
+		catch (ArithmeticException e) {
+			System.err.println("ERROR: Arithmetic Exception");
+		}
+	}
+	
+	public void nullPointer() {
+		Integer [] arr = new Integer [10];
+		try {
+			int z = arr[0].intValue();
+		} 
+		catch (NullPointerException e) {
+			System.err.println("ERROR: NullPointerException");
+		}
+	}
+	
+	public void indexBounds() {
+		String hello = "Hello";
+		try {
+			char c = hello.charAt(hello.length());
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("ERROR: IndexOutOfBoundsException");
+		}
+	}
+	
+	/**
+	 * Exceptions have hierarchy - super, sub
+	 * ArrayIndexOutOfBoundsException is a sub-exception to IndexOutOfBoundsException
+	 * So, you can catch IndexOutOfBoundsException or ArrayIndexOutOfBoundsException for the next line
+	 */ 
+	public void arrayIndexBounds() {
+		int [] arr = new int [10];
+		try {
+			int k = arr[10];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("ERROR: ArrayIndexOutOfBoundsException");
+		}
+	}
+	
+	public void concurrentModification() {
+		List<Integer> arr = new ArrayList<Integer>(); 
+		// change array list's size
+		
+		for (int a = 0; a < 5; a++) {
+			arr.add(a + 1);
+		}
+		
+		try {
+			for (Integer x: arr) {
+				arr.remove(0);
+			}
+		} catch (ConcurrentModificationException e) {
+			System.err.println("ERROR: ConcurrentModificationException");
+		}
+	}
+	
+}
